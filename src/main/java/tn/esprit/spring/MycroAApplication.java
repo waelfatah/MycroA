c68815690365ac1729d5f3afd923639cd14a24b9
Wael Fatah <wael.fatah@esprit.tn>
2021-03-11 23:23:11 +0100

    First Commit

---
+++ src/main/java/tn/esprit/spring/MycroAApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycroAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycroAApplication.class, args);
+	}
+
+}
